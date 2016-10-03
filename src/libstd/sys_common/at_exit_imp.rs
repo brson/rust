abed31f79e4da17a5a7d8f0c7a9d7fd8850e19f9
Brian Anderson <banderson@mozilla.com>
2016-10-03 00:05:28 +0000

    Refactor at_exit more

--- src/libstd/sys_common/at_exit_imp.rs
+++ src/libstd/sys_common/at_exit_imp.rs
@@ -16,21 +16,6 @@ use alloc::boxed::FnBox;
 use ptr;
 use sys::mutex::Mutex;
 
-type Queue = Vec<Box<FnBox()>>;
-
-// NB these are specifically not types from `std::sync` as they currently rely
-// on poisoning and this module needs to operate at a lower level than requiring
-// the thread infrastructure to be in place (useful on the borders of
-// initialization/destruction).
-static LOCK: Mutex = Mutex::new();
-static mut QUEUE: *mut Queue = ptr::null_mut();
-
-// The maximum number of times the cleanup routines will be run. While running
-// the at_exit closures new ones may be registered, and this count is the number
-// of times the new closures will be allowed to register successfully. After
-// this number of iterations all new registrations will return `false`.
-const ITERS: usize = 10;
-
 /// Enqueues a procedure to run when the main thread exits.
 ///
 /// Currently these closures are only run once the main *Rust* thread exits.
@@ -42,51 +27,87 @@ const ITERS: usize = 10;
 /// that the closure could not be registered, meaning that it is not scheduled
 /// to be run.
 pub fn at_exit<F: FnOnce() + Send + 'static>(f: F) -> Result<(), ()> {
-    if push(Box::new(f)) {Ok(())} else {Err(())}
-}
-
-fn push(f: Box<FnBox()>) -> bool {
-    let mut ret = true;
-    unsafe {
-        LOCK.lock();
-        if init() {
-            (*QUEUE).push(f);
-        } else {
-            ret = false;
-        }
-        LOCK.unlock();
-    }
-    ret
-}
-
-unsafe fn init() -> bool {
-    if QUEUE.is_null() {
-        let state: Box<Queue> = box Vec::new();
-        QUEUE = Box::into_raw(state);
-    } else if QUEUE as usize == 1 {
-        // can't re-init after a cleanup
-        return false
-    }
-
-    true
+    unsafe { AT_EXIT.at_exit(f) }
 }
 
 pub fn cleanup() {
-    for i in 0..ITERS {
+    unsafe { AT_EXIT.cleanup() }
+}
+
+static mut AT_EXIT: AtExit = AtExit::new();
+
+// The maximum number of times the cleanup routines will be run. While running
+// the at_exit closures new ones may be registered, and this count is the number
+// of times the new closures will be allowed to register successfully. After
+// this number of iterations all new registrations will return `false`.
+const ITERS: usize = 10;
+
+type Queue = Vec<Box<FnBox()>>;
+
+// NB these are specifically not types from `std::sync` as they currently rely
+// on poisoning and this module needs to operate at a lower level than requiring
+// the thread infrastructure to be in place (useful on the borders of
+// initialization/destruction).
+struct AtExit {
+    lock: Mutex,
+    queue: *mut Queue
+}
+
+impl AtExit {
+    pub const fn new() -> AtExit {
+        AtExit {
+            lock: Mutex::new(),
+            queue: ptr::null_mut()
+        }
+    }
+
+    pub fn at_exit<F: FnOnce() + Send + 'static>(&mut self, f: F) -> Result<(), ()> {
+        if self.push(Box::new(f)) {Ok(())} else {Err(())}
+    }
+
+    fn push(&mut self, f: Box<FnBox()>) -> bool {
+        let mut ret = true;
         unsafe {
-            LOCK.lock();
-            let queue = QUEUE;
-            QUEUE = if i == ITERS - 1 {1} else {0} as *mut _;
-            LOCK.unlock();
+            self.lock.lock();
+            if self.init() {
+                (*self.queue).push(f);
+            } else {
+                ret = false;
+            }
+            self.lock.unlock();
+        }
+        ret
+    }
 
-            // make sure we're not recursively cleaning up
-            assert!(queue as usize != 1);
+    unsafe fn init(&mut self) -> bool {
+        if self.queue.is_null() {
+            let state: Box<Queue> = box Vec::new();
+            self.queue = Box::into_raw(state);
+        } else if self.queue as usize == 1 {
+            // can't re-init after a cleanup
+            return false
+        }
 
-            // If we never called init, not need to cleanup!
-            if queue as usize != 0 {
-                let queue: Box<Queue> = Box::from_raw(queue);
-                for to_run in *queue {
-                    to_run();
+        true
+    }
+
+    pub fn cleanup(&mut self) {
+        for i in 0..ITERS {
+            unsafe {
+                self.lock.lock();
+                let queue = self.queue;
+                self.queue = if i == ITERS - 1 {1} else {0} as *mut _;
+                self.lock.unlock();
+
+                // make sure we're not recursively cleaning up
+                assert!(queue as usize != 1);
+
+                // If we never called init, not need to cleanup!
+                if queue as usize != 0 {
+                    let queue: Box<Queue> = Box::from_raw(queue);
+                    for to_run in *queue {
+                        to_run();
+                    }
                 }
             }
         }
